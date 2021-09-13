7ee840d9c128b7bf4fb2a21066786478e9463da3
lrbarros <luizricardobarros@gmail.com>
2021-09-13 08:04:20 -0300

    commit

--- contatos/lib/ui/contact_page.dart
+++ contatos/lib/ui/contact_page.dart
@@ -2,17 +2,34 @@ import 'package:contatos/helper/contact_helper.dart';
 import 'package:flutter/material.dart';
 
 class ContactPage extends StatefulWidget {
-  final Contact contact;
+  final Contact? contact;
 
-  ContactPage(this.contact);
+  ContactPage({this.contact});
 
   @override
   _ContactPageState createState() => _ContactPageState();
 }
 
 class _ContactPageState extends State<ContactPage> {
+  Contact? _editedContact;
+
+  @override
+  void initState() {
+      super.initState();
+
+      if(widget.contact == null){
+        _editedContact = Contact();
+      }else{
+        //duplica o contato (copia) 
+        _editedContact = Contact.fromMap( widget.contact!.toMap());
+      }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
+
+
 }
